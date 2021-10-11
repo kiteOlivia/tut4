ace4db16e1181cb80a5cd0d73b9fc087927b4cff
Oliviabalabala <1653713864@qq.com>
2021-10-11 20:41:32 +0800

    tut4

---
+++ src/main/java/tut4/Calc.java
@@ -0,0 +1,16 @@
+package tut4;
+
+public class Calc {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println(add(a,b));
+
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static int subtract(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/tut4/TestCalc.java
@@ -0,0 +1,18 @@
+package tut4;
+import org.junit.Assert;
+import org.junit.Test;
+public class TestCalc {
+ @Test
+    public void addTest(){
+     Calc c = new Calc();
+     int add  = c.add(10,10);
+     Assert.assertEquals(20,add);
+ }
+    @Test
+    public void subtractTest(){
+        Calc s = new Calc();
+        int subtract  = s.subtract(10,10);
+        Assert.assertEquals(20,subtract);
+    }
+
+}
